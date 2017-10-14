b1205ab12edb61553bde3f4af67b0716c6a69fb3
Nicolas San Jose <nicolasasanjose@gmail.com>
2017-10-14 11:36:28 -1000

    added collapsable within projects

--- js/indexJS.js
+++ js/indexJS.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
 
-	// navigation button press
-	$(".nav-link").on("click", function(){
-		// switch active button
+	function switchActiveButton(targetButton) {
 		$(".navbar-nav").find(".active").removeClass("active");
-		$(this).parent().addClass("active");
+		targetButton.addClass("active");
+	}
 
-		// switch open accordion section
-		var targetID = $(this).attr("data-sectionID");
+	function switchActiveSection(targetID)
+	{
 		var activeSection = $(".section").filter(function(index) { return $(this).hasClass("activeSection"); });
 		if (activeSection.attr("id") != targetID)
 		{
@@ -15,10 +14,31 @@ $(document).ready(function() {
 			$(".section").filter(function(index) { return $(this).attr("id") == targetID; })
 				.addClass("activeSection");
 		}
+	}
 
+	function toggleActiveSection(targetID)
+	{
+		var targetSection = $("#"+targetID);
+		if (targetSection.hasClass("activeSection"))
+		{
+			targetSection.removeClass("activeSection");
+		} else
+		{
+			targetSection.addClass("activeSection");
+		}
+	}
+
+	// navigation button press - page switch
+	$(".nav-link").on("click", function(){
+		switchActiveButton($(this).parent());
+		switchActiveSection($(this).attr("data-sectionID"));
 		// collapse mobile menu on selection
 		$(".navbar-collapse").collapse('hide');
 	});
 
+	// toggle - project descriptions
+	$(".proj-link").on("click", function(){
+		toggleActiveSection($(this).attr("data-sectionID"));
+	});
 });
 
